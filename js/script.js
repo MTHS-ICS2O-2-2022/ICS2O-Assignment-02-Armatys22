a421a1caf6651e20505926c0ac2ba32947a2fa7f
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-03-12 20:59:03 +0000

    march break work

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("add-math").innerHTML = "<p>The area is:" + (11+11) + "cm</p>"
+  document.getElementById("subtract-math").innerHTML = "The perimeter is:  " + (7 - 3) + "cm</p>"  
+  document.getElementById("multiply-math").innerHTML = "<p>The volume is: " + (9*9+2) + "cm</p>" 
+}
